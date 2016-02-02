454be67a21db5e69bce38ce4c3df50b4f7fec4e8
Jarred Capellman <jcapellman@hotmail.com>
2016-02-01 22:15:05 -0500

    Added ability to add and delete feeds

--- jcRSS.UWP/ViewModels/ManageFeedsViewModel.cs
+++ jcRSS.UWP/ViewModels/ManageFeedsViewModel.cs
@@ -23,7 +23,18 @@ namespace jcRSS.UWP.ViewModels {
             set { _feedSiteURL = value; RaisePropertyChanged("FeedSiteURL"); }
         }
 
+        private List<FeedSiteItem> _selectedFeedSites;
+
+        public List<FeedSiteItem> SelectedFeedSites {
+            get { return _selectedFeedSites; }
+            set { _selectedFeedSites = value; RaisePropertyChanged("SelectedFeedSites"); }
+        } 
+         
         private FeedList _feedList;
+
+        public ManageFeedsViewModel() {
+            SelectedFeedSites = new List<FeedSiteItem>();
+        }
 
         public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state) {
             var feeds = await _FileSystem.GetFile<FeedList>(FILE_TYPES.FEED_LIST);
@@ -49,12 +60,28 @@ namespace jcRSS.UWP.ViewModels {
             FeedSiteListing = new ObservableCollection<FeedSiteItem>(_feedList.FeedSites);
         }
 
+        public async void RemoveFeedSites() {
+            foreach (var feedSite in SelectedFeedSites) {
+                _feedList.FeedSites.Remove(feedSite);
+            }
+
+            await _FileSystem.WriteFile(FILE_TYPES.FEED_LIST, _feedList);
+
+            SelectedFeedSites.Clear();
+
+            FeedSiteListing = new ObservableCollection<FeedSiteItem>(_feedList.FeedSites);
+        }
+
         public async void AddFeedSite() {
             var feedSite = await _rssClient.GetFeedInformation(FeedSiteURL);
 
             _feedList.FeedSites.Add(feedSite);
 
-            var result = await _FileSystem.WriteFile(FILE_TYPES.FEED_LIST, _feedList);            
+            var result = await _FileSystem.WriteFile(FILE_TYPES.FEED_LIST, _feedList);
+
+            FeedSiteListing = new ObservableCollection<FeedSiteItem>(_feedList.FeedSites);
+
+            FeedSiteURL = string.Empty;
         }
     }
 }
--- jcRSS.UWP/Views/ManageFeeds.xaml.cs
+++ jcRSS.UWP/Views/ManageFeeds.xaml.cs
@@ -1,27 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using jcRSS.PCL.Objects.Feeds;
 
 namespace jcRSS.UWP.Views {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class ManageFeeds : Page {
         public ManageFeeds() {
             this.InitializeComponent();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e) {
+            ViewModel.SelectedFeedSites = lvFeeds.SelectedItems.Select(a => (FeedSiteItem)a).ToList();
+
+            ViewModel.RemoveFeedSites();
         }
     }
 }
