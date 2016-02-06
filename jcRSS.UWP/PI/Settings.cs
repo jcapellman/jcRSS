876b2c861407aa717ff4fe209fee3b9134696358
Jarred Capellman <jcapellman@hotmail.com>
2016-02-06 08:15:36 -0800

    Settings are now loaded and saved to the roamingsettings

--- jcRSS.UWP/PI/Settings.cs
+++ jcRSS.UWP/PI/Settings.cs
@@ -1,12 +1,23 @@
-﻿using jcRSS.PCL.Objects.Common;
+﻿using System;
+
+using jcRSS.PCL.Enums;
+using jcRSS.PCL.Objects.Common;
 using jcRSS.PCL.PA;
 
 namespace jcRSS.UWP.PI {
     public class Settings : BaseSettings {
         private readonly Windows.Storage.ApplicationDataContainer _localSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
 
+        private SETTINGS convertString(string key) {
+            return (SETTINGS) Enum.Parse(typeof (SETTINGS), key);
+        }
+
         public override void LoadSettings() {
             _SettingsContainer = new SettingsContainer();
+
+            foreach (var setting in _localSettings.Values) {
+                _SettingsContainer.WriteSetting(convertString(setting.Key), setting.Value);
+            }
         }
 
         public override void WriteSettings() {
